9cac812a8f56c8364c337f1f972c714331dfa5cb
Marco Fuentes <marcofuentes1607@gmail.com>
2019-11-11 09:34:42 -0600

    Notebook updated

---
+++ graphs.py
@@ -0,0 +1,32 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = 0.2;
+l = 0.3;
+g = 9.8;
+k = 0.9;
+
+def theta_1(t,c_1,c_2):
+	return c_1*math.sin(math.sqrt(g/l)*t)+c_2*math.cos(math.sqrt(g/l)*t)
+
+def theta_2(t,c_1,c_2):
+	return c_1*math.cos(math.sqrt((g/l)+(2*k/(m)))*t + c_2)
+t = np.arange(0,5*3.14,0.01)
+t_1 = []
+t_2 = []
+t_3 = []
+for i in t: t_1.append(theta_1(i,0.7,0.7))
+
+for j in t: t_2.append(theta_2(j,1,0))
+
+for k in range(len(t)): t_3.append(t_1[k]+t_2[k])
+plt.title('Modos normales de oscilación del sistema')
+plt.plot(t,t_1,'b--',label='Modo 1')
+plt.plot(t,t_2,'r--',label='Modo 2')
+plt.plot(t,t_3,'g',label='Combinacion lineal de modos normales')
+plt.ylabel('Posicion angular (rad)')
+plt.xlabel('Tiempo (s)')
+plt.legend(loc='upper right')
+plt.ylim(-3,3.5)
+plt.show()
--- proyectoEcuas.py
+++ proyectoEcuas.py
@@ -25,9 +25,12 @@ def xdot(t,x,y,theta,phi):
 	return phi*(-(g/l) - k/m) + theta*k/m
 
 def RungeKutta(thetadot, ydot, phidot, xdot,tI, thetai, yi, phii, xi, tF, h):
+	#Como es una aproximación numérica, el resultado se guarda en arreglos de datos tipo numérico.
+	# El primer elemento de cada arreglo representa las condiciones iniciales.
 	t,theta,y,phi,x = [tI] ,[thetai] ,[yi] ,[phii], [xi]
 	i = 0
 	while t[i] <= tF:
+		#Se calculan los valores de K respectivos asociados a cada ecuación
 		k1theta = thetadot	(t[i],x[i],y[i],theta[i],phi[i])
 		k1y = ydot			(t[i],x[i],y[i],theta[i],phi[i])
 		k1phi = phidot		(t[i],x[i],y[i],theta[i],phi[i])
@@ -48,6 +51,7 @@ def RungeKutta(thetadot, ydot, phidot, xdot,tI, thetai, yi, phii, xi, tF, h):
 		k4phi = phidot		(t[i]+h/2,	x[i]+(h/2)*k3x	,y[i]+(h/2)*k3y	,theta[i]+(h/2)*k3theta	,phi[i]+(h/2)*k3phi)
 		k4x = xdot			(t[i]+h/2,	x[i]+(h/2)*k3x	,y[i]+(h/2)*k3y	,theta[i]+(h/2)*k3theta	,phi[i]+(h/2)*k3phi)
 
+		#Los valores calculados se agregan
 		x.append(x[i] + h*(k1x + 2*k2x + 2*k3x + k4x)/6)
 		y.append(y[i] + h*(k1y + 2*k2y + 2*k3y + k4y)/6)
 		theta.append(theta[i] + h*(k1theta + 2*k2theta + 2*k3theta + k4theta)/6)
@@ -58,10 +62,17 @@ def RungeKutta(thetadot, ydot, phidot, xdot,tI, thetai, yi, phii, xi, tF, h):
 	return t,theta,phi
 
 t,theta,phi = RungeKutta(thetadot, ydot, phidot,xdot,0,3.14/6,0,0,0,4*3.14,0.0001)
-plt. plot(t, theta,'r')
-plt.plot(t, phi,'b')
+
+tst = []
+for i in range(len(t)):
+    tst.append(theta[i]+phi[i])
+
+#plt.plot(t, tst,'g--')
+plt. plot(t, theta,'r--',label='Posición de la masa 1')
+plt.plot(t, phi,'b',label='Posición de la masa 2')
 plt.ylabel('Posicion angular (rad)')
 plt.xlabel('Tiempo (s)')
 plt.title('Posicion de los pendulos')
-
+plt.legend(loc='upper right')
+plt.ylim(-0.8,0.8)
 plt.show()
